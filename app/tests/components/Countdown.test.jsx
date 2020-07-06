71e496a92ad4c70e71477dff8a8f38a38d359981
Diego Klappenbach <dklappenbach@vairix.com>
2020-07-06 18:06:50 -0300

    Add countdown tests and countdown header

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -24,6 +24,7 @@ describe('Countdown', () => {
         done();
       }, 1001);
     });
+    
     it('does not set the count to less than 0 when finishing the countdown', done => {
       const countdown = TestUtils.renderIntoDocument(<Countdown/>);
       countdown.handleCountdown(1);
@@ -33,6 +34,31 @@ describe('Countdown', () => {
         done();
       }, 3000);
     });
+
+    it('pauses countdown on paused status', done => {
+      const countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('stops countdown on stopped status', done => {
+      const countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleCountdown(15);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
+    });
+
   })
 
 });
